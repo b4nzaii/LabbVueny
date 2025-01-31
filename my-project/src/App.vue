640559696ed6dfc6c5846f19d0f0e061a5a4be89
b4nzaii <Mans200@live.se>
2025-01-31 15:09:07 +0100

    lagt till enkel navbar skiss med sökruta med knapp

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -4,6 +4,10 @@
       <router-link to="/games">Games</router-link>
       <router-link to="/about">About</router-link>
     </nav>
+    <div class="search-bar">
+      <input v-model="SearchQuery" type="text" placeholder="Sök efter spel">
+      <button @click="searchGames">Search</button>
+    </div>
   </header>
   <main>
 <router-view />
@@ -14,3 +18,43 @@ export default{
   name: 'App'
 }
 </script>
+<style scoped>
+
+.app-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family:'Courier New', Courier, monospace;
+  background-color: white;
+}
+
+header {
+  width: 100%;
+  background-color: black;
+  padding: 15px 5px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+nav {
+  display: flex;
+  gap: 1em;
+}
+
+nav a {
+  color: white;
+  font-size: 25px;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+nav a:hover {
+  color: #1279e7;
+  text-decoration: underline;
+}
+
+main {
+  width: 80%;
+}
+</style>
